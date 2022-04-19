9fd7b3c5d82ca488afe6aca08b218618accfac5f
abutler2 <42008138+abutler2@users.noreply.github.com>
2022-04-18 21:15:44 -0400

    Added camera follow changer

--- Studio 1 Game/Assets/Scripts/CameraFollow.cs
+++ Studio 1 Game/Assets/Scripts/CameraFollow.cs
@@ -12,9 +12,55 @@ public class CameraFollow : MonoBehaviour
 
     public Transform player;
     public Vector3 offset;
+    public bool followX, followXandY, followXandZ,followAll;
 
     void Update()
     {
-        transform.position = new Vector3(player.position.x + offset.x,  offset.y, offset.z); // Camera follows the player with specified offset position
+        if(followX){
+            transform.position = new Vector3(player.position.x + offset.x, offset.y, offset.z); // Camera follows the player with specified offset position
+        }
+        if (followXandY)
+        {
+            transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
+        }
+        if (followXandZ)
+        {
+            transform.position = new Vector3(player.position.x + offset.x, offset.y, player.position.z + offset.z); // Camera follows the player with specified offset position
+        }
+        if (followAll)
+        {
+            transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, player.position.z + offset.z); // Camera follows the player with specified offset position
+        }
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "followX")
+        {
+            followX = true;
+            followXandY = false;
+            followXandZ = false;
+            followAll = false;
+        }
+        if (other.tag == "followXandY")
+        {
+            followX = false;
+            followXandY = true;
+            followXandZ = false;
+            followAll = false;
+        }
+        if (other.tag == "followXandZ")
+        {
+            followX = false;
+            followXandY = false;
+            followXandZ = true;
+            followAll = false;
+        }
+        if (other.tag == "followAll")
+        {
+            followX = false;
+            followXandY = false;
+            followXandZ = false;
+            followAll = true;
+        }
     }
 }
