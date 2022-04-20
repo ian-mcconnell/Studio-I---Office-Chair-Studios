b0e36afbfea4410f0154ebb3eecb86cb30684a82
abutler2 <42008138+abutler2@users.noreply.github.com>
2022-04-19 21:40:51 -0400

    Added MainMenu
    and small changes to other scenes

---
+++ Studio 1 Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void NewGame()
+    {
+        //we have to add our scenes to the buildsettings for this to work
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void LoadGame()
+    {
+        PlayerData data = SaveSystem.loadPlayer();
+        //we have to add our scenes to the buildsettings for this to work
+        SceneManager.LoadScene(data.level);
+        Debug.Log(data.level);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("quit the game!(as an application)");
+        Application.Quit();
+    }
+}
---
+++ Studio 1 Game/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for(int i = 0; i< resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+    public void SetQuality(int qualityLevel)
+    {
+        QualitySettings.SetQualityLevel(qualityLevel);
+    }
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+        Debug.Log("toggled fullscreen (when built)");
+    }
+    public void SetRes(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
