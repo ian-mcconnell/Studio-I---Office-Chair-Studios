d013b32fe77c2f340eb3dbb071fcec93ef89b157
Ian <ianmcconnell376@gmail.com>
2022-05-02 02:15:54 -0400

    Hedge Fight Nearly Finished
    Still needs a navmesh and basketball/damage

--- Studio 1 Game/Assets/Scripts/Enemies/EnemyHedge.cs
+++ Studio 1 Game/Assets/Scripts/Enemies/EnemyHedge.cs
@@ -7,16 +7,16 @@ public class EnemyHedge : Enemy
     public Transform[] waypoints;
     private int waypointItterator = 0;
     Transform targetWaypoint;
+    private float bufferDistance = 6f;
 
     private HedgeStates stateCurrent = HedgeStates.Idling;
 
-    public float attackRange = 2f;
-    public float aggroRange = 5f;
-
     public GameObject whirlAttack;
+    public GameObject basketballObj;
+    public Transform[] ballSpawns;
 
-    private int attackCooldown = 0;
-    private bool attackLocked = false;
+    private int attackCooldown = 6;
+    private int whirlCount = 0;
 
     private ParticleSystem ps;
     private SpriteRenderer sr;
@@ -63,21 +63,21 @@ public class EnemyHedge : Enemy
         {
             case HedgeStates.Idling:
                 if (isDead) { StateIdlingExit(); StateDeadEnter(); }
-                else if (playerDistance <= attackRange) { StateIdlingExit(); StateHoopingEnter(); }
-                else if (playerDistance <= aggroRange) { StateIdlingExit(); StateWhirlingEnter(); }
+                else if (whirlCount >= 3 && attackCooldown == 0) { StateIdlingExit(); StateHoopingEnter(); }
+                else if (whirlCount == 0 && attackCooldown == 0) { StateIdlingExit(); StateWhirlingEnter(); }
                 else { StateIdlingRemain(); }
                 break;
 
             case HedgeStates.Whirling:
                 if (isDead) { StateWhirlingExit(); StateDeadEnter(); }
-                else if (playerDistance <= attackRange) { StateWhirlingExit(); StateIdlingEnter(); }
+                else if (whirlCount >= 3) { StateWhirlingExit(); StateIdlingEnter(); }
                 else { StateWhirlingRemain(); }
                 break;
 
             case HedgeStates.Hooping:
                 if (isDead) { StateHoopingExit(); StateDeadEnter(); }
-                else if (playerDistance >= attackRange && playerDistance < aggroRange && !attackLocked) { StateHoopingExit(); StateIdlingEnter(); }
-                else { StateHoopingExit(); }
+                else if (whirlCount == 0) { StateHoopingExit(); StateIdlingEnter(); }
+                else { StateHoopingRemain(); }
                 break;
 
             case HedgeStates.Dead:
@@ -90,22 +90,21 @@ public class EnemyHedge : Enemy
     void StateIdlingEnter()
     {
         stateCurrent = HedgeStates.Idling;
-        if (Vector3.Distance(targetWaypoint.position, transform.position) <= 1f)
+
+        if (attackCooldown > 0)
         {
-            waypointItterator += 1;
+            attackCooldown--;
         }
-        targetWaypoint = waypoints[waypointItterator % waypoints.Length];
-        base.agent.destination = targetWaypoint.position;
+        
+        base.agent.destination = transform.position;
     }
 
     void StateIdlingRemain()
     {
-        if (Vector3.Distance(targetWaypoint.position, transform.position) <= 1f)
+        if (attackCooldown > 0)
         {
-            waypointItterator += 1;
+            attackCooldown--;
         }
-        targetWaypoint = waypoints[waypointItterator % waypoints.Length];
-        base.agent.destination = targetWaypoint.position;
     }
 
     void StateIdlingExit()
@@ -116,55 +115,48 @@ public class EnemyHedge : Enemy
     void StateWhirlingEnter()
     {
         stateCurrent = HedgeStates.Whirling;
-        base.agent.destination = player.position;
+        base.animator.SetBool("isWhirling", true);
+        base.agent.destination = targetWaypoint.position;
+
+        StartCoroutine(WhirlAttack());
     }
 
     void StateWhirlingRemain()
     {
-        base.agent.destination = player.position;
+        if (Vector3.Distance(targetWaypoint.position, transform.position) <= bufferDistance)
+        {
+            waypointItterator++;
+            whirlCount++;
+            if (whirlCount != 3)
+            {
+                targetWaypoint = waypoints[waypointItterator % waypoints.Length];
+                base.agent.destination = targetWaypoint.position;
+            }
+        }
     }
 
     void StateWhirlingExit()
     {
-
+        base.animator.SetBool("isWhirling", false);
+        attackCooldown = 11;
     }
 
     void StateHoopingEnter()
     {
         stateCurrent = HedgeStates.Hooping;
-        if (attackCooldown == 0)
-        {
-            int randInt = Random.Range(1, 3);
-            if (randInt == 1)
-            {
-                StartCoroutine(SmallAttack());
-            }
-            else
-            {
-                //StartCoroutine(LargeAttack());
-            }
-        }
+        base.animator.SetBool("isHooping", true);
+        
+        StartCoroutine(HoopAttack());
     }
 
     void StateHoopingRemain()
     {
-        if (attackCooldown == 0)
-        {
-            int randInt = Random.Range(1, 3);
-            if (randInt == 1)
-            {
-                StartCoroutine(SmallAttack());
-            }
-            else
-            {
-                //StartCoroutine(LargeAttack());
-            }
-        }
+       
     }
 
     void StateHoopingExit()
     {
-
+        base.animator.SetBool("isWhirling", false);
     }
 
     void StateDeadEnter()
@@ -179,63 +171,51 @@ public class EnemyHedge : Enemy
 
     }
 
-    IEnumerator SmallAttack()
+    IEnumerator HoopAttack()
     {
-        Debug.Log("Small Attack");
-        attackLocked = true;
-        attackCooldown = 19;
-        base.animator.SetBool("attackingWeak", true);
+        Debug.Log("Hoop Attack");
 
         yield return new WaitForSeconds(.6f);
-        if (sr.flipX == false)
+        
+        foreach (Transform tr in ballSpawns)
         {
-            //Instantiate(smallAttack, leftAttackSpawn.position, new Quaternion(0, 0, 0, 0));
-        }
-        else
-        {
-            //Instantiate(smallAttack, leftAttackSpawn.position, new Quaternion(0, 0, 0, 0));
+            Instantiate(basketballObj, tr.position, new Quaternion(0, 0, 0, 0));
+            yield return new WaitForSeconds(.1f);
         }
 
         yield return new WaitForSeconds(.3f);
 
-        attackLocked = false;
-        base.animator.SetBool("attackingWeak", false);
+        attackCooldown = 11;
+        whirlCount = 0;
+
         yield return null;
     }
 
     IEnumerator WhirlAttack()
     {
-        Debug.Log("Large Attack");
-        attackLocked = true;
-        attackCooldown = 35;
-        base.animator.SetBool("attackingStrong", true);
-        var targetTemp = base.agent.destination;
-        base.agent.destination = transform.position;
+        Debug.Log("Whirl Attack");
 
-        yield return new WaitForSeconds(1.2f);
-        if (sr.flipX == false)
+        while (stateCurrent == HedgeStates.Whirling)
         {
-            //Instantiate(largeAttack, leftAttackSpawn.position, new Quaternion(0, 0, 0, 0));
-        }
-        else
-        {
-            //Instantiate(largeAttack, leftAttackSpawn.position, new Quaternion(0, 0, 0, 0));
+            Instantiate(whirlAttack, transform.position, new Quaternion(0, 0, 0, 0));
+            yield return new WaitForSeconds(.2f);
         }
 
-        yield return new WaitForSeconds(.1f);
+        attackCooldown = 11;
 
-        attackLocked = false;
-        base.agent.destination = targetTemp;
-        base.animator.SetBool("attackingStrong", false);
         yield return null;
     }
 
+
     public override void ChangeHealth(float amount)
     {
-        base.ChangeHealth(amount);
-        if (amount < 0)
+        if (stateCurrent != HedgeStates.Whirling)
         {
-            ps.Play();
+            base.ChangeHealth(amount);
+            if (amount < 0)
+            {
+                ps.Play();
+            }
         }
     }
 }
