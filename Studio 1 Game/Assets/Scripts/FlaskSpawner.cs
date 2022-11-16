7b375577447a21857f54c82eb07b041568f3d04b
Ian <ianmcconnell376@gmail.com>
2022-11-15 22:19:07 -0500

    Flasks and spawner stuff

---
+++ Studio 1 Game/Assets/Scripts/FlaskSpawner.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlaskSpawner : MonoBehaviour
+{
+    public float initialDelay = 0f;
+    public float repeatDelay = 0f;
+    private bool spawning = true;
+    public GameObject flaskPrefab;
+
+    void Start()
+    {
+        Invoke("SpawnFlask", initialDelay);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player") && !spawning)
+        {
+            spawning = true;
+            Invoke("SpawnFlask", repeatDelay);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && !spawning)
+        {
+            spawning = true;
+            Invoke("SpawnFlask", repeatDelay);
+        }
+    }
+
+    private void SpawnFlask()
+    {
+        spawning = false;
+        Instantiate(flaskPrefab);
+    }
+}
