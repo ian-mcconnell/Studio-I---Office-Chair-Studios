b77a6fb35ee54a8eac4d3ba37f6d210775d39020
abutler2 <42008138+abutler2@users.noreply.github.com>
2022-12-06 23:23:34 -0500

    New Game Warning

--- Studio 1 Game/Assets/Scripts/MainMenu.cs
+++ Studio 1 Game/Assets/Scripts/MainMenu.cs
@@ -5,13 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
+    public GameObject SaveWarningText;
+    public bool WarningAcknowledged;
+    private void Start()
+    {
+        WarningAcknowledged = false;
+        Time.timeScale = 1f;
+    }
     public void NewGame()
     {
         PlayerData data = SaveSystem.loadPlayer();
-        data.level = 1;
-        //we have to add our scenes to the buildsettings for this to work
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if(data.level != 1)
+        {
+            StartCoroutine(SaveWarning());
+            if (WarningAcknowledged)
+            {
+                data.level = 1;
+                //we have to add our scenes to the buildsettings for this to work
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+        else
+        {
+            data.level = 1;
+            //we have to add our scenes to the buildsettings for this to work
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+
+        
     }
     public void LoadGame()
     {
@@ -19,6 +41,18 @@ public class MainMenu : MonoBehaviour
         //we have to add our scenes to the buildsettings for this to work
         SceneManager.LoadScene(data.level);
         Debug.Log(data.level);
+
+    }
+    IEnumerator SaveWarning()
+    {
+        SaveWarningText.SetActive(true);
+        yield return new WaitForSeconds(5);
+        SaveWarningText.SetActive(false);
+    }
+    public void SaveWarningButton()
+    {
+        WarningAcknowledged = true;
+        NewGame();
     }
     public void QuitGame()
     {
